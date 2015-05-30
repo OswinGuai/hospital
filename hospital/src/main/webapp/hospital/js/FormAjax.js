03272bc979088987b16329b5dd0bee016d0acadf
sunkang <sunkang@c52fcabc-473c-8d49-9d85-b277cd0628f3>
2015-05-30 02:45:03 +0000

    添加身份证验证方法。

--- hospital/src/main/webapp/hospital/js/FormAjax.js
+++ hospital/src/main/webapp/hospital/js/FormAjax.js
@@ -52,6 +52,8 @@ $(document).ready(function () {
             idcard: "29348388384"
 }]
     };
+ GetContactsList($("#ContactList"), result.data, "name", "idcard", "id");
+
 
     //如果返回状态正常获取数据并操作
     if (getDataStatus == "0") {
@@ -137,8 +139,81 @@ $(document).ready(function () {
     var contactList = $("#ContactList");
     GetContactsList(contactList, SuffererListResult, "sufferers", "SuffererName", "SuffererPhone", "SuffererId");*/
 
-
 });
+
+var CheckIdCard = {
+    //Wi 加权因子 Xi 余数0~10对应的校验码 Pi省份代码
+    Wi: [7, 9, 10, 5, 8, 4, 2, 1, 6, 3, 7, 9, 10, 5, 8, 4, 2],
+    Xi: [1, 0, "X", 9, 8, 7, 6, 5, 4, 3, 2],
+    Pi: [11, 12, 13, 14, 15, 21, 22, 23, 31, 32, 33, 34, 35, 36, 37, 41, 42, 43, 44, 45, 46, 50, 51, 52, 53, 54, 61, 62, 63, 64, 65, 71, 81, 82, 91],
+
+    //检验18位身份证号码出生日期是否有效
+    //parseFloat过滤前导零，年份必需大于等于1900且小于等于当前年份，用Date()对象判断日期是否有效。
+    brithday18: function (sIdCard) {
+        var year = parseFloat(sIdCard.substr(6, 4));
+        var month = parseFloat(sIdCard.substr(10, 2));
+        var day = parseFloat(sIdCard.substr(12, 2));
+        var checkDay = new Date(year, month - 1, day);
+        var nowDay = new Date();
+        if (1900 <= year && year <= nowDay.getFullYear() && month == (checkDay.getMonth() + 1) && day == checkDay.getDate()) {
+            return true;
+        };
+    },
+
+    //检验15位身份证号码出生日期是否有效
+    brithday15: function (sIdCard) {
+        var year = parseFloat(sIdCard.substr(6, 2));
+        var month = parseFloat(sIdCard.substr(8, 2));
+        var day = parseFloat(sIdCard.substr(10, 2));
+        var checkDay = new Date(year, month - 1, day);
+        if (month == (checkDay.getMonth() + 1) && day == checkDay.getDate()) {
+            return true;
+        };
+    },
+
+    //检验校验码是否有效
+    validate: function (sIdCard) {
+        var aIdCard = sIdCard.split("");
+        var sum = 0;
+        for (var i = 0; i < CheckIdCard.Wi.length; i++) {
+            sum += CheckIdCard.Wi[i] * aIdCard[i]; //线性加权求和
+        };
+        var index = sum % 11; //求模，可能为0~10,可求对应的校验码是否于身份证的校验码匹配
+        if (CheckIdCard.Xi[index] == aIdCard[17].toUpperCase()) {
+            return true;
+        };
+    },
+
+    //检验输入的省份编码是否有效
+    province: function (sIdCard) {
+        var p2 = sIdCard.substr(0, 2);
+        for (var i = 0; i < CheckIdCard.Pi.length; i++) {
+            if (CheckIdCard.Pi[i] == p2) {
+                return true;
+            };
+        };
+    }
+};
+
+function Validata(obj) {
+    var sIdCard = obj.value.replace(/^\s+|\s+$/g, ""); //去除字符串的前后空格，允许用户不小心输入前后空格
+    if (sIdCard.match(/^\d{14,17}(\d|X)$/gi) == null) { //判断是否全为18或15位数字，最后一位可以是大小写字母X
+        alert("身份证号码须为18位或15位数字"); //允许用户输入大小写X代替罗马数字的Ⅹ
+    } else if (sIdCard.length == 18) {
+        if (CheckIdCard.province(sIdCard) && CheckIdCard.brithday18(sIdCard) && CheckIdCard.validate(sIdCard)) {
+            alert("身份证号码合法");
+        } else {
+            alert("请输入有效的身份证号码");
+        };
+    } else if (sIdCard.length == 15) {
+        if (CheckIdCard.province(sIdCard) && CheckIdCard.brithday15(sIdCard)) {
+            alert("身份证号码合法");
+        } else {
+            alert("请输入有效的身份证号码");
+        };
+    };
+};
+
 
 //填充下拉列表数据（select id,数据源，json指定对象，key，value）
 function FillSelectData(listid, data, object, value, text) {
@@ -188,19 +263,19 @@ function SaveData() {
 
 
 //患者列表
-function GetContactsList(listid, data, object, name, phone, suffererid) {
-        //清空Contacts ListItem
-        RemoveContacts(listid);
+function GetContactsList(listid, data, name, IdCard, id) {
+    //清空Contacts ListItem
+    RemoveContacts(listid);
 
-        var ContactName = "";
-        var ContactPhone = "";
-        $(data[object]).each(function (key) {
-            //添加联系人
-            ContactName = data[object][key][name];
-            ContactPhone = data[object][key][phone];
-            listid.append("<li><a href='ContactDetail.html?ContactId=" + data[object][key][suffererid] + "&action=view'><span class='ContactName'>" + ContactName + "</span><span class='ContactPhone'>" + ContactPhone + "</span></a></li>");
-        });
-    }
+    var ContactName = "";
+    var ContactPhone = "";
+    $(data).each(function (key) {
+        //添加联系人
+        ContactName = data[key][name];
+        ContactIdCard = data[key][IdCard];
+        listid.append("<li><a href='ContactDetail.html?ContactId=" + data[key][id] + "&action=view'><span class='ContactName'>" + ContactName + "</span><span class='ContactIdCard'>" + ContactIdCard.substring(0, 4) + "****" + ContactIdCard.substring(ContactIdCard.length - 4, ContactIdCard.length) + "</span></a></li>");
+    });
+}
 
 //清空患者列表
 function RemoveContacts(listid) {
